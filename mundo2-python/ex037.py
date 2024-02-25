768d2097c313d9a135111db2eab9a74f8b221e01
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-02-25 12:30:38 -0300

    final ex37

--- mundo2-python/ex037.py
+++ mundo2-python/ex037.py
@@ -2,17 +2,17 @@ print('='*30,'Conversor de Base Numerica','='*30)
 
 numero = int(input('Qual numero voce deseja converter: '))
 base = int(input('Qual a base de conversão?\n1 Para binario\n2 Para Octal\n3 para hexadecimal\n'))
-contador = 1
-calculo1 = numero / 2  
 
 if base==1:
-    print('binario')
-    while numero / 2 == 0 or numero / 2 == 1: 
-        print(calculo1 % 2)
-        contador + 1
+    numero_binario = bin(numero)[2:]
+    print('O numero BINARIO é: {}' .format(numero_binario))
+
 elif base==2:
-    print('octal')
+    numero_octa = oct(numero)[2:]
+    print('O numero OCTAL é: {}' .format(numero_octa))
 elif base==3:
-    print('hexadecimal')
+    numero_hexa = hex(numero)[2:]
+    print('O numero HEXADECIMAL é: {}' .format(numero_hexa.upper()))
+    
 else:
     print('Insira um numero de 1 a 3')
