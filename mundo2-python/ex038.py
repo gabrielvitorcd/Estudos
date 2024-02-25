2b7be4e206ce645f43b6b32ad476e783d7fedd64
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-02-25 13:46:03 -0300

    exercicio 38

--- mundo2-python/ex038.py
+++ mundo2-python/ex038.py
@@ -1,1 +1,11 @@
-print()
+print('='*30, 'Comparador de Numeros', '='*30)
+
+num1 = int(input('Insira o numero 1: '))
+num2 = int(input('Insira o numero 2: '))
+
+if num1 > num2:
+    print('O numero 1: {} é maior que o Numero 2: {}' .format(num1, num2))
+elif num2 > num1:
+    print('O numero 2: {} é maior que o Numero 1: {}' .format(num2, num1))
+else:
+    print('Os dois valores são iguais')
