4afbf9af614814e925853f2a52b6df55806d5c63
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-02-25 09:26:31 -0300

    Cor de letra ex001.py

--- mundo1-python/ex001.py
+++ mundo1-python/ex001.py
@@ -1,3 +1,3 @@
 print('============ Desafio 02 ============')
 nome = input('Qual seu nome?')
-print('É um prazer te conhecer, {}!' .format(nome))
+print('É um prazer te conhecer, \033[32m{}\033[m!' .format(nome))
