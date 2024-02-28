b7e8e2236844b8c67c05c0977fd7deb9a4cf01f5
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-02-27 23:02:55 -0300

    final ex 45

--- mundo2-python/ex045.py
+++ mundo2-python/ex045.py
@@ -1,32 +1,21 @@
-import time
 from random import randint
-print('='*30, 'JOGO JOKENPO', '='*30)
 
-
-usuario = int(input('Selecione:\n1- Pedra\n2- Papel\n3- Tesoura\n'))
-maquina = randint(1, 3)
-
-
-print('Processando...')
-time.sleep(1)
-
-if usuario == 1:
-    print('Voce1: PEDRA')
-elif usuario == 2:
-    print('Voce: PAPEL')
-elif usuario == 3:
-    print('Voce: TESOURA')
-
-if maquina == 1:
-    print('O Pc: PEDRA')
-elif maquina == 2:
-    print('O Pc: PAPEL')
-elif maquina == 3:
-    print('O Pc: TESOURA')
+itens  = ('Pedra', 'Papel' , 'Tesoura')
+maquina = randint (0, 2)
+usuario = int(input('''
+[ 0 ]- Pedra
+[ 1 ]- Papel
+[ 2 ]- Tesoura
+Qual a sua jogada? 
+'''))
+print('-=' * 15)
+print('O computador escolheu: {}' .format(itens[maquina]))
+print('Voce escolheu: {}' .format(itens[usuario]))
+print('-=' * 15)
 
 if usuario == maquina:
     print('Empate!')
-elif (usuario == 1 and maquina == 3) or (usuario == 2 and maquina == 1) or (usuario == 3 and maquina == 2):
-    print('Você ganhou!')
+elif (usuario == 0 and maquina == 2) or (usuario == 2 and maquina == 1) or (usuario == 1 and maquina == 0):
+    print('VOCE GANHOU DA MAQUINA!')
 else:
-    print('Você perdeu!')
+    print('A MAQUINA GANHOU VOCE!')
