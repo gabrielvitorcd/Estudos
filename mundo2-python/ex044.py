ab0ee2c7b771b43cd35d73f7c689c654d575c6fd
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-02-27 22:28:36 -0300

    final ex 044

--- mundo2-python/ex044.py
+++ mundo2-python/ex044.py
@@ -1,8 +1,12 @@
 print('='*30, 'PROGRAMA QUE ANALISA A FORMA DE PAGAMENTO E CALCULA O VALOR FINAL', '='*30)
 
 valor = float(input('Insira o valor do produto: '))
-
-print('Qual forma de pagamento:\n1- Avista Dinheiro ou Cheque\n2- Avista Cartão\n3- Parcelado 2 vezes\n4- Parcelado 3 ou mais')
+print(''' FORMAS DE PAGAMENTO
+      [ 1 ] a vista dinheiro/cheque
+      [ 2 ] a vista cartao
+      [ 3 ] 2x no cartao
+      [ 4 ] 3x ou mais no cartao
+''')
 
 formapagamento = int(input('Insira a sua forma de Pagamento: '))
 
@@ -14,8 +18,11 @@ elif formapagamento == 2:
     print('Voce ganhou 5% de desconto: R${:.2f}' .format(precofinal))
 elif formapagamento == 3:
     print('Voce vai pagar o valor normal do produto: R${:.2f}' .format(valor))
+    print('Parcelado em 2x: R${:.2f}' .format(valor / 2)) 
 elif formapagamento == 4:
     precofinal = valor + (valor*0.2)
+    parcela = int(input('Insira Quantas Parcelas: '))
     print('Voce vai pagar 20% de juros: R${:.2f}' .format(precofinal))
+    print('Voce parcelou em {}x: R${:.2f}' .format(parcela, precofinal / parcela))
 else:
     print('Insira uma opção valida')
