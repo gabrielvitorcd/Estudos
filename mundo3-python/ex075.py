63715d438341d2267d3d44976932fa575e5da691
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-03-04 10:12:46 -0300

    ex 75 correcao

--- mundo3-python/ex075.py
+++ mundo3-python/ex075.py
@@ -1,7 +1,17 @@
-tupla = ()
+pares = 0
+num = (int(input('Digite um numero: ')),
+       int(input('Digite um numero: ')),
+       int(input('Digite um numero: ')),
+       int(input('Digite um numero: ')),)
+   
+for n in num:
+    if n % 2 == 0:
+        pares+=1
 
-for c in range(1,5):
-    num = int(input('Digite um numero: '))
-    tupla += (num,)
-    
-print(tupla)
+print(f'Voce digitou os valores {num}')
+print(f'O valor 9 apareceu {num.count(9)} vezes')
+if 3 in num:
+    print(f'O valor 3 apareceu na {num.index(3)+1} posicao')
+else:
+    print('Nenhum valor 3 foi digitado')
+print(f'Os valores pares digitados foram {pares}')
