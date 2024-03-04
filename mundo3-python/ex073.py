3d5d9e1f27cbc4a0f3e26a0e23e8228b40b3de1d
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-03-03 22:16:03 -0300

    Update ex073.py

--- mundo3-python/ex073.py
+++ mundo3-python/ex073.py
@@ -5,11 +5,15 @@ times = (
     "Cruzeiro", "CSA", "Chapecoense", "Avaí"
 )
 
-listaclassifica = sorted(times)
-
+print('-='*30)
+print(f'A classificao final de 2019: {times}')
+print('-='*30)
 print(f'Apenas os 5 primeiros colocados {times[0:5]}')
+print('-='*30)
 print(f'Os ultimos 4 colocados da tabela {times[-4:]}')
-print(f'Uma lista com os times em ordem alfabetica {listaclassifica}')
-print(f'Em que posicao está o time da chapecoense {times.index("Chapecoense")}') # O Index so procura com aspas dupla
+print('-='*30)
+print(f'Uma lista com os times em ordem alfabetica {sorted(times)}')
+print('-='*30)
+print(f'O chapecoense está na {times.index("Chapecoense")+1} posicao') # O Index so procura com aspas dupla
 
-      
+     
