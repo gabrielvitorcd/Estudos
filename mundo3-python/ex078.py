b9f2a75ecc27669836a8697b94ec55e48e171827
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-03-06 08:35:36 -0300

    resolucao gv ex78

--- mundo3-python/ex078.py
+++ mundo3-python/ex078.py
@@ -1,14 +1,33 @@
 valores = []
-for leitura in range (1,6):
-    valores.append(int(input('Digite um numero: ')))
+menor = 0
+maior = 0
+posicaomaior = 0
+posicaomenor = 0
 
-print(f'Os valores sao: {valores}')
+for contador in range(0,5):
+    valores.append(int(input(f'Digite o valor {contador}: ')))
+
+for chave, valor in enumerate(valores):
+        
+    if chave == 0:
+        maior = valor
+        posicaomaior = chave
+        menor = valor
+        posicaomenor = chave
+    else:
+        if valor > maior:
+            maior = valor
+            posicaomaior = chave
+        if valor < menor:
+            menor = valor
+            posicaomenor = chave
+           
+        
+
+print('-='*30)
+print(f'Voce digitou os valores {valores}')
+print(f'O maior valor é {maior} na posicao {posicaomaior}')
+print(f'O menor valor é {menor} na posicao {posicaomenor}')
 
 
-for c, v in enumerate(valores):
-    valores.sort(reverse=True)
-    print(f'O maior valor é {v} na posicao {c}')
-    print(f'O menor valor é {valores[-1]}')
-
-print(f'{valores}')
     
