20af3be3b747cfff69e9ea784de23a47b4088be7
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-03-08 17:05:16 -0300

    resolucao gv ex 87

--- mundo3-python/ex087.py
+++ mundo3-python/ex087.py
@@ -1,14 +1,24 @@
 todosnum = [[],[],[]]
 somapar = 0
+somacoluna = 0
+maior = 0
 
 for c in range(0,3):
     num = int(input(f'Digite um valor para [0, {c}]: '))
+    if num % 2 == 0:
+        somapar += num
     todosnum[0].append(num)
 for c in range(0,3):
     num = int(input(f'Digite um valor para [1, {c}]: '))
+    if num > maior:
+        maior = num
+    if num % 2 == 0:
+        somapar += num
     todosnum[1].append(num)
 for c in range(0,3):
     num = int(input(f'Digite um valor para [2, {c}]: '))
+    if num % 2 == 0:
+        somapar += num
     todosnum[2].append(num)
 
 print('-='*30)
@@ -25,7 +35,8 @@ for n in todosnum[2]:
     print(f'[ {n} ]', end='')
 print()
 
-for n in todosnum:
-    if n % 2 == 0:
-        somapar += n
 print(f'a soma dos todos os numeros pares é {somapar}')
+for s in todosnum:
+    somacoluna += s[2]
+print(f'A soma dos valores da terceira coluna é {somacoluna}')
+print(f'O maior valor da segunda linha é {maior}')
