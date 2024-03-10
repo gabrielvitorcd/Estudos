0a352e5f6a3066a45ec6787d21fb5cb429419859
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-03-10 08:46:17 -0300

    Update ex089.py

--- mundo3-python/ex089.py
+++ mundo3-python/ex089.py
@@ -4,11 +4,9 @@ while True:
     dado.append(str(input('Nome: ')))
     dado.append(float(input('Nota 1: ')))
     dado.append(float(input('Nota 2: ')))
-
-
     alunos.append(dado[:])
     dado.clear()
-    
+
     saida = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     while saida not in 'SN':
         saida = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
@@ -23,14 +21,15 @@ print('-'*25)
 
 while True:
     mostrar = int(input('Mostrar notas de qual aluno? (999 interrompe): '))
-    
+
     if mostrar == 999:
-        break    
-    
+        break
+
     while mostrar > (len(alunos) - 1):
-        mostrar = int(input('Esse aluno nao existe, digite novamente. (999 interrompe): '))
+        mostrar = int(
+            input('Esse aluno nao existe, digite novamente. (999 interrompe): '))
         if mostrar == 999:
-            break   
+            break
 
     print(f'Notas de {alunos[mostrar][0]} sao {alunos[mostrar][1:]}')
 
