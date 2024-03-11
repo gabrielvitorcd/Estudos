e5aaee3fd08a5ad3437877b5c5071a8b290ab255
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-03-11 07:48:59 -0300

    ex92

--- mundo3-python/ex092.py
+++ mundo3-python/ex092.py
@@ -2,11 +2,17 @@ from datetime import date
 
 pessoa = {}
 
-pessoa['nome: '] = str(input('Nome: '))
+pessoa['nome'] = str(input('Nome: '))
 ano = int(input('Ano de Nascimento: '))
 pessoa['idade'] = date.today().year - ano
-
-
-
-
-print(pessoa)
+pessoa['ctps'] = int(input('Carteira de trabalho (0 nao tem): '))
+print('-='*30)
+if pessoa['ctps'] == 0:
+    for k, v in pessoa.items():
+        print(f'- {k} tem o valor {v}')
+else:
+    pessoa['contratacao'] = int(input('Ano de contratacao: '))
+    pessoa['salario'] = float(input('Salario: R$'))
+    pessoa['aposentadoria'] = pessoa['contratacao'] + 35 - ano
+    for k, v in pessoa.items():
+        print(f'- {k} tem o valor {v}')
