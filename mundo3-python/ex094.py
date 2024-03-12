f53aff0ec25351187461a52bb5490b207c913c94
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-03-12 13:17:24 -0300

    resolucaogv ex94

--- mundo3-python/ex094.py
+++ mundo3-python/ex094.py
@@ -1,24 +1,20 @@
 cadastro = dict()
 pessoa = list()
 soma = 0
-soma2 = 0
 while True:
 
     cadastro['nome'] = str(input('Nome: '))
-    
+
     cadastro['sexo'] = str(input('Sexo: [M/F] ')).strip().upper()[0]
     while cadastro['sexo'] not in 'MF':
         print('ERRO! Por favor, digite apenas M ou F')
         sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
-    
+
     cadastro['idade'] = int(input('Idade: '))
     soma += cadastro['idade']
 
     pessoa.append(cadastro.copy())
     cadastro.clear()
-
-
-
 
     resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     while resp not in 'SN':
@@ -26,10 +22,19 @@ while True:
         resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     if resp == 'N':
         break
-print(f'Ao todo temos {len(pessoa)} pessoas cadastradas')
-print(f'A media de idade é de {soma/len(pessoa)}')
-for c in pessoa:   #assisit aula do for e estudar
-    print(pessoa[c]['idade'])
+media = soma/len(pessoa)
 
-print(soma2)
-print(cadastro)
+print(f'A) Ao todo temos {len(pessoa)} pessoas cadastradas')
+print(f'B) A media de idade é de {media:.1f}')
+print('C) As mulheres cadastradas foram ', end='')
+for c in pessoa:
+    if c['sexo'] in 'F':
+        print(c['nome'], end=' ')
+print()
+print('D) Listas das pessoas que estao acima da media: ')
+for l in pessoa:
+    if l['idade'] > media:
+        for k, v in l.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+print('Encerrado!')
