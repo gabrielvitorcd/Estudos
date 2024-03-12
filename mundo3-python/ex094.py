72e23db2d589ea2c7d9b1354df818f5a1c49c246
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-03-12 12:25:48 -0300

    Update ex094.py

--- mundo3-python/ex094.py
+++ mundo3-python/ex094.py
@@ -1,15 +1,19 @@
 cadastro = dict()
 pessoa = list()
-
+soma = 0
+soma2 = 0
 while True:
 
     cadastro['nome'] = str(input('Nome: '))
+    
     cadastro['sexo'] = str(input('Sexo: [M/F] ')).strip().upper()[0]
     while cadastro['sexo'] not in 'MF':
         print('ERRO! Por favor, digite apenas M ou F')
         sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
-    cadastro['idade'] = int(input('Idade: '))
     
+    cadastro['idade'] = int(input('Idade: '))
+    soma += cadastro['idade']
+
     pessoa.append(cadastro.copy())
     cadastro.clear()
 
@@ -22,6 +26,10 @@ while True:
         resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     if resp == 'N':
         break
+print(f'Ao todo temos {len(pessoa)} pessoas cadastradas')
+print(f'A media de idade é de {soma/len(pessoa)}')
+for c in pessoa:   #assisit aula do for e estudar
+    print(pessoa[c]['idade'])
 
-print(pessoa)
+print(soma2)
 print(cadastro)
