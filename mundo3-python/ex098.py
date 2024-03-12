1d5ce6ff19952c0d8d75118de111f766a1cd9e83
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-03-12 19:34:51 -0300

    Update ex098.py

--- mundo3-python/ex098.py
+++ mundo3-python/ex098.py
@@ -4,7 +4,7 @@ def contador(i, f, p):
     print(f'Contagem de {i} até {f} de {p} em {p}')
     for c in range(i, f+1, p):
         print(f' {c}', end='->')
-        time.sleep(1)
+        
     print(' FIM!')
         
 
