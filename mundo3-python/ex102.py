91789ae0c992e41eb09182a6f535cc34cc636cfd
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-03-13 20:41:49 -0300

    Update ex102.py

--- mundo3-python/ex102.py
+++ mundo3-python/ex102.py
@@ -7,14 +7,15 @@ def fatorial(n, show=False):
     """  
     f = 1          
     for c in range(n, 0 , -1): 
-        if show:
+        if show:        #Se show 
             print(c,end='')
-        if c > 1:
-            print(' x ',end='')
-        else:
-            print(' = ',end='')
+            if c > 1:
+                print(' x ',end='')
+            else:
+                print(' = ',end='')
         f *= c               
     return f
 
 print(fatorial(5,True))
+print(fatorial(3))
 
