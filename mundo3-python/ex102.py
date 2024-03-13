b593313adc3fa61fdc13713bb4dfb6d1b2fd2b3f
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-03-13 20:37:11 -0300

    resolucao guanabara ex 102

--- mundo3-python/ex102.py
+++ mundo3-python/ex102.py
@@ -1,5 +1,20 @@
-def fatorial(num = 1, show = False):          #Fatorial de 5:      5 * 4 * 3 * 2 * 1 = 120
-    f = 1           #Valor Inicial
-    for c in range(num, 0 , -1): #Laco que comeca em num e depois vai lendo de tras para frente
-        f *= c              #Opera dentro do laco multiplicando o valor 1 inicial por cada numero do laco 
-    return f            #retorna o valor fatorial do numero desejado
+def fatorial(n, show=False):
+    """
+    -> Calcula o Fatorial de um numero
+    :parametro n: O numero que vai ser calculado
+    :parametro show: (opcional) Mostrar ou nao a conta.
+    :return: O valor do fatorial de um numero n.      
+    """  
+    f = 1          
+    for c in range(n, 0 , -1): 
+        if show:
+            print(c,end='')
+        if c > 1:
+            print(' x ',end='')
+        else:
+            print(' = ',end='')
+        f *= c               
+    return f
+
+print(fatorial(5,True))
+
