c22d977215bcdedacbed67c27c76e2e1cebe9a89
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-03-13 23:05:39 -0300

    help def ex 105

--- mundo3-python/ex105.py
+++ mundo3-python/ex105.py
@@ -1,4 +1,11 @@
-def notas(*num):
+def notas(*num,sit = False):
+    """
+    notas(*n , sit=False)
+        ->Funcao para analisar notas e situacoes de varios alunos.
+        :parametro n: uma ou mais notas dos alunos (aceita varias)
+        :parametro sit: Valor opcional,indicando se deve ou nao adicionar a situacao
+        :return: dicionario com varias informacoes sobre a situacao da turma.    
+     """
     resumo = {}
     maior = menor = 0
     for c in num:
@@ -12,8 +19,12 @@ def notas(*num):
     resumo['maior'] = maior
     resumo['menor'] = menor
     resumo['media'] = sum(num)/len(num)
- 
+    if sit == True:
+        if resumo['media'] >= 7:
+            resumo['situacao'] = 'Aprovado'
+        else:
+            resumo['situacao'] = 'Reprovado'
     return resumo
 
-resp = notas(5.5,2.5,10,6.5)
+resp = notas(5.5,2.5,10,6.5,)
 print(resp)
