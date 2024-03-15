52014ce2ea80ddb782635726aae4f9edf2d623b6
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-03-14 21:50:13 -0300

    pacote moedas

---
+++ mundo3-python/moedas.py
@@ -0,0 +1,15 @@
+def metade(n):
+    return n / 2
+
+def dobro(n):
+    return n * 2
+
+def aumentar(n,adi):
+    adi /= 100
+    return n + (n * adi)
+
+def diminuir(n, sub):
+    sub /= 100
+    return n - (n * sub)
+
+
