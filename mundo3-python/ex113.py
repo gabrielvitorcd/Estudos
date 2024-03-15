20eacaaf2d8e14db40e1e0924bf5a436c007af72
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-03-15 15:31:31 -0300

    Update ex113.py

--- mundo3-python/ex113.py
+++ mundo3-python/ex113.py
@@ -6,9 +6,15 @@ def leiaInt():
     try:
         int = int(input('Digite um Inteiro: '))
         return int          #return cria um break no laco
-    except:
+    except(ValueError,TypeError):
         print(f'{cores.selecionarcor()[1]}ERRO!{cores.selecionarcor()[0]} Digite um número inteiro válido.')
         continue
+    except KeyboardInterrupt:
+       print(f'\n{cores.selecionarcor()[1]} O usuario preferiu terminar o programa {cores.selecionarcor()[0]}')
+       int = 0
+       return int
+       
+   
 
 def leiaFloat():
    global flot
