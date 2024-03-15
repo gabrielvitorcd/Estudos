06a2fa3625e2f8f6e9766cde88fde0c1566f275a
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-03-14 23:08:36 -0300

    ex108

--- mundo3-python/ex108.py
+++ mundo3-python/ex108.py
@@ -1,7 +1,7 @@
 import moeda
 
 p = float(input('Digite o preco: R$ '))
-print(f'A metade {p} é {moeda.formatacao(moeda.metade(p))}')
-print(f'O dobro de {p} é {moeda.formatacao(moeda.dobro(p))}')
-print(f'Aumentando 10%, temos {moeda.formatacao(moeda.aumentar(p,10))}')
-print(f'Reduzindo 13%, temos {moeda.formatacao(moeda.diminuir(p,13))}')
+print(f'A metade {p} é {moeda.metade(p,False)}')
+print(f'O dobro de {p} é {moeda.dobro(p,True)}')
+print(f'Aumentando 10%, temos {moeda.aumentar(p,10,True)}')
+print(f'Reduzindo 13%, temos {moeda.diminuir(p,13,True)}')
--- mundo3-python/moeda.py
+++ mundo3-python/moeda.py
@@ -1,17 +1,36 @@
-def metade(n):
-    return n / 2
+def metade(n,format=False):
+    met = n /2
+    if format == False:
+        return met
+    if format == True:
+        return f'R${met:.2f}'
 
-def dobro(n):
-    return n * 2
+def dobro(n,format=False):
+    dob = n * 2 
+    if format == False:
+        return dob
+    if format == True:
+        return f'R${dob:.2f}'
 
-def aumentar(n,adi):
+def aumentar(n,adi,format=False):
     adi /= 100
-    return n + (n * adi)
+    aumento = n + (n * adi)
+    if format == False:
+        return aumento
+    if format == True:
+        return f'R${aumento:.2f}'
 
-def diminuir(n, sub):
+def diminuir(n, sub,format=False):
     sub /= 100
-    return n - (n * sub)
+    dimi = n - (n * sub)
+    if format == False:
+        return n - (n * sub)
+    if format == True:
+        return f'R${dimi:.2f}'
 
-def formatacao(n):
-    return f'R${n:.2f}'
+def formatacao(ft):
+    return f'R${ft:.2f}'
+        
+
+
 
