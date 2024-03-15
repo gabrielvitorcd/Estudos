9d5152287ab7a14a1942313670d42ba7b89efa31
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-03-15 03:39:56 -0300

    final

--- mundo3-python/utilidadescev/dado/__init__.py
+++ mundo3-python/utilidadescev/dado/__init__.py
@@ -1,6 +1,6 @@
 
 def leiaDinheiro(msg):
-    n = str(input(msg)).strip()
+    n = str(input(msg)).strip().replace(',','.')
     while not n.isnumeric():
         print(f'ERRO: "{n}" é um preco invalido')
         n = str(input(msg)).strip()
