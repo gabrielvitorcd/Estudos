ad6a075eb5d341de439d3af3935d81cba3285217
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-03-29 15:23:16 -0300

    aula78

---
+++ otm/sec4/aula78b.py
@@ -0,0 +1,6 @@
+s1 = {1,2,3}
+
+print(4 in s1) #output: False
+
+for numero in s1:
+    print(numero)
---
+++ otm/sec4/aula78c.py
@@ -0,0 +1,9 @@
+# metodos do set
+
+s1 = set()
+s1.add(1)   # .add  -> so adiciona um valor por vez
+s1.add(1.5)
+s1.add('ABC')
+print(s1)   #set nao garante a ordem
+
+s1.update('Gabriel ')    #adiciona mais valores de uma vez
