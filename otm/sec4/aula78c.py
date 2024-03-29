febfded9caef1038796f0c48efa22cd3369bc882
Gabriel Vitor <gabriel.vitor03@gmail.com>
2024-03-29 15:33:06 -0300

    Update aula78c.py

--- otm/sec4/aula78c.py
+++ otm/sec4/aula78c.py
@@ -6,4 +6,9 @@ s1.add(1.5)
 s1.add('ABC')
 print(s1)   #set nao garante a ordem
 
-s1.update('Gabriel ')    #adiciona mais valores de uma vez
+s1.update(('Gabriel',15,35))    #adiciona mais valores de uma vez
+print(s1)
+
+s1.discard(35)  #descarta um valor do set
+s1.discard('Gabriel')
+print(s1)
