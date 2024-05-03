ab4eba9cfd028223acb17ed663d7aed4826ff43e
Gabriel Vitor <161081860+gabrielvitorcd@users.noreply.github.com>
2024-05-03 17:47:15 -0300

    funcao open e exercicio

---
+++ otm/sec4/aula119.py
@@ -0,0 +1,47 @@
+# Exercício - Lista de tarefas com desfazer e refazer
+# Música para codar =)
+# Everybody wants to rule the world - Tears for fears
+# todo = [] -> lista de tarefas
+# todo = ['fazer café'] -> Adicionar fazer café
+# todo = ['fazer café', 'caminhar'] -> Adicionar caminhar
+# desfazer = ['fazer café',] -> Refazer ['caminhar']
+# desfazer = [] -> Refazer ['caminhar', 'fazer café']
+# refazer = todo ['fazer café']
+# refazer = todo ['fazer café', 'caminhar'
+
+
+lista_completa = []
+lista_edicao = []
+
+
+while True:
+    print('Comandos: listar, desfazer, refazer')
+    resp_usuario = input('Digite uma tarefa ou comando: ')
+    
+    if resp_usuario == 'listar':
+        print('TAREFAS:\n')
+        for tarefa in lista_completa:
+            print(tarefa)
+        continue
+    if resp_usuario == 'desfazer':
+        lista_edicao.append(lista_completa[-1])
+        lista_completa.pop()
+        continue
+
+    if resp_usuario == 'refazer':
+        if len(lista_edicao) == 0:
+            print('NADA PRA REFAZER MANO')
+        lista_completa.append(lista_edicao[-1])
+        lista_edicao.pop()
+        continue
+    
+    if resp_usuario	== 'sair':
+        break
+    
+    try:
+        lista_completa.append(resp_usuario)
+    except:
+        print('deu ruim mano')
+    
+    
+    
