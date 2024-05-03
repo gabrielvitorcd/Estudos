338d8753cafee4540a4f11eefadafb8d2ca3aad6
Gabriel Vitor <161081860+gabrielvitorcd@users.noreply.github.com>
2024-05-03 12:39:08 -0300

    Update aula115b.py

--- otm/sec4/aula115b.py
+++ otm/sec4/aula115b.py
@@ -9,3 +9,7 @@
 # Congelar (ver pacotes)
 # pip freeze
 
+# Criando e usando um requirements.txt
+# pip freeze > requirements.txt
+# Instalando tudo do requirements.txt
+# pip install -r requirements.txt
