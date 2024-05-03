492d9f295001356b56f4edf53eb129d2559a9e00
Gabriel Vitor <161081860+gabrielvitorcd@users.noreply.github.com>
2024-05-03 12:33:18 -0300

    ambiente virtual(venv)

---
+++ otm/sec4/aula115a.py
@@ -0,0 +1,23 @@
+# Ambientes virtuais em Python (venv)
+# Um ambiente virtual carrega toda a sua instalação
+# do Python para uma pasta no caminho escolhido.
+# Ao ativar um ambiente virtual, a instalação do
+# ambiente virtual será usada.
+# venv é o módulo que vamos usar para
+# criar ambientes virtuais.
+# Você pode dar o nome que preferir para um
+# ambiente virtual, mas os mais comuns são:
+# venv env .venv .env
+
+
+#Comando para criar um ambiente virtual no powershell
+# python -m venv venv
+
+#Comando gcm para mostrar qual local ele está
+# gcm python -Syntax
+
+#comando para ativar o venv
+# venv\Scripts\activate
+
+#comando para desativar o venv
+# venv\Scripts\deactivate
